37bb9c190c243d6b8389a8315d8f8eb3b8bd2342
Aravind6-46 <155824374+Aravind6-46@users.noreply.github.com>
2024-01-09 14:26:31 +0530

    First Cmmit

---
+++ src/Bond/James.java
@@ -0,0 +1,35 @@
+package Bond;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class James {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://practice.automationtesting.in/my-account/");
+		
+		driver.manage().window().maximize();
+				
+		WebElement username =driver.findElement(By.xpath("//input[@id=\"username\"]"));
+		
+		username.sendKeys("t.aravind646@gmail.com");
+		
+		WebElement password = driver.findElement(By.xpath("//input[@id=\"password\"]"));
+		
+		password.sendKeys("Aravind@123");
+		
+		WebElement sign = driver.findElement(By.xpath("(//input[@type=\"submit\"])[1]"));
+		
+		sign.click();
+		
+		String actualUrl="https://practice.automationtesting.in/my-account/";
+		
+		String expectedUrl= driver.getCurrentUrl();
+	}
+
+}
